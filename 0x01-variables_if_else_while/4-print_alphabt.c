d96f9756e1f65b1090f865fd1ccae162806f3f4f
sh-mak <shadicj30@gmail.com>
2023-06-19 19:09:41 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all except e and q
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+			ch++;
+	}
+	putchar('\n');
+	return (0);
+}
