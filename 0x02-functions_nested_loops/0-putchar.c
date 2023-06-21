381c67811804e1fb63cf8ec38dad499bae57757d
sh-mak <shadicj30@gmail.com>
2023-06-21 16:03:03 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main Entry point
+ *
+ * Description:  program that prints _putchar, followed by a new line
+ *
+*/
+int main(void)
+{
+	 printf("_putchar\n");
+
+	return (0);
+}
