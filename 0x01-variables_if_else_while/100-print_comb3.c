675839885c708d27f8dd2aab3676e4276d5f1c21
sh-mak <shadicj30@gmail.com>
2023-06-25 17:14:24 -0700

    y

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- *main Entry point
- *
- *Description: prints all possible different combinations of two digits
+ *main - prints possible different combinations of two digits
  *
  *Return: Always 0 (Success)
 */
