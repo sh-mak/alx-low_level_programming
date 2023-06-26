6b72ee1a14fe8b44ab6978a887ba79a8b15d719f
sh-mak <shadicj30@gmail.com>
2023-06-25 17:00:49 -0700

    y

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 /**
  *main Entry point
  *
- *Description: print all single digit numbers of base 10 starting from 0
+ *Description: print single digit numbers of base 10
  *
  *Return: Always 0 (Success)
 */
