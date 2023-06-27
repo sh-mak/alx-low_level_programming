d2c70acb09adad6ae0645811ee7c2b299373dc4a
sh-mak <shadicj30@gmail.com>
2023-06-27 05:18:42 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
-int _puchar (char c);
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -16,4 +16,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
