d7165c870732971d877433254c2585748053cd97
sh-mak <shadicj30@gmail.com>
2023-06-28 06:41:13 -0700

    rev-str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,12 @@
 
 void rev_string(char *s)
 {
-        int len = 0, index = 0;
-        char temp;
-        while (s[index++])
-                len++;
+       	int len = 0, index = 0;
+	char temp;
+	while (s[index++])
+		len++;
 
-        for (index = len - 1; index >= len / 2; index--)
+	for (index = len - 1; index >= len / 2; index--)
 	{
 		temp = s[index];
 		s[index] = s[len - index - 1];
