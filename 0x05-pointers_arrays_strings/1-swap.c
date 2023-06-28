7d914d25d524e3a8da9c3bab0e514664abdcd66f
sh-mak <shadicj30@gmail.com>
2023-06-27 18:45:16 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * swap _ int - swaps the values of two integers
+ * Descriptin - swaps the values of two integers
  * using two input parameters
  * @a: input parameter 1
  * @b: input parameter 2
  * Return: nothing
  */
 void swap_int(int *a, int *b)
-/* the functoin swap_int  that swaps the values of two integers */
+
 {
 	int temp;
 
