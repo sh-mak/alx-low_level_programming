16faba6cb06ad3987ef1b2f5dfad9c4f324a9924
sh-mak <shadicj30@gmail.com>
2023-06-27 18:47:24 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Descriptin - swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * using two input parameters
  * @a: input parameter 1
  * @b: input parameter 2
