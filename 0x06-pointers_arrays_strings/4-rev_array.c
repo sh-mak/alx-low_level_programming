e01396e7af8fe0578d66826fb07b87348f882523
sh-mak <shadicj30@gmail.com>
2023-06-30 04:03:39 -0700

    rev-array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * revers_array - revers array of integer
+ * @a: array
+ * @n: number of elementsof array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
