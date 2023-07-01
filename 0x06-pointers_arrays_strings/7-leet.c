aa58e6092ad889a2b65cc2b40a7e7ad01c6eb8ed
sh-mak <shadicj30@gmail.com>
2023-06-30 17:08:57 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,15 +10,15 @@ char *leet(char *n)
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	 for (i = 0; n[i] != '\0'; i++)
-	 {
+	for (i = 0; n[i] != '\0'; i++)
+	{
 		for (j = 0; n[j] < 10; j++)
-		 {
-			 if (n[i] == s1[j])
-			 {
-				 n[i] = s2[j];
-			 }
-		 }
-	 }
-	 return (n);
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
 }
