6deb505283faac49e0b712e669387a7937373516
sh-mak <shadicj30@gmail.com>
2023-07-02 07:54:44 -0700

    alphab

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- *
- * prints 10 times the alphabet, in lowercase, followed by a new line.
+ *print_alphabet_x10 - function that prints 10 times the alphabet
 */
 void print_alphabet_x10(void)
 {
