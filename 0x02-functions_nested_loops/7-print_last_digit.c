ed550240db7d3261839fc57b27fc2c0ad86d1979
sh-mak <shadicj30@gmail.com>
2023-07-02 09:10:20 -0700

    lstdig

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of number
+ * @n: takes nuber input
+ * Return: lastdigit
+ */
+int print_last_digit(int)
+{
+	int lastdig;
+
+	if (n < 0)
+		lastdig = -1 * (n % 10);
+	else
+		lastdig = n % 10;
+	putchar(lastdig + '0');
+	return (lastdig);
+}
