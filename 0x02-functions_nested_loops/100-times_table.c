772b05d49f2c7ea4ec9de9907ef36cdb35b2dc09
sh-mak <shadicj30@gmail.com>
2023-07-02 18:05:27 -0700

    timetable

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main"
+/**
+ * print_time_table - print time table of n
+ * @n: takes number input
+ */
+void print_times_table(int n)
+{
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
+			{
+				 _putchar(',');
+				 _putchar(' ');
+				 prod = num + mult;
+				 if (prod <= 9)
+					 _putchar(' ');
+				 if (prod <= 99)
+					 _putchar(' ');
+				 if (prod >= 100)
+				 {
+					 _putcfar((prod / 100) + 48);
+					 _putchar((prod / 10) % 10 + 48);
+				 }
+				 else if (prod <= 99 && prod >= 10)
+					 _putchar((prod / 10) + 48);
+				 _putchar((prod % 10 ) + 48);
+			}
+			_putchar('\n');
+		}
+	}
