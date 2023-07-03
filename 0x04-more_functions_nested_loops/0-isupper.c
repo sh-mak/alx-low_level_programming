205800b73baaa016e0f43c4d121dfbbb09eed811
sh-mak <shadicj30@gmail.com>
2023-07-03 16:34:00 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _isupper - checks for uppercase character
- * @x: the check  number
+ * @c: the check  number
  * Return: 1 for upper letter and 0  for else
  */
 int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
-		retur (1);
+		return (1);
 	}
 	return (0);
 }
