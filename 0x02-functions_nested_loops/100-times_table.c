ae9c3f1bf4feee317978eee26a48f1b0488308ff
sh-mak <shadicj30@gmail.com>
2023-07-02 18:11:32 -0700

    timetable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,7 @@ void print_times_table(int n)
 					 _putchar(' ');
 				 if (prod >= 100)
 				 {
-					 _putcfar((prod / 100) + 48);
+					 _putchar((prod / 100) + 48);
 					 _putchar((prod / 10) % 10 + 48);
 				 }
 				 else if (prod <= 99 && prod >= 10)
