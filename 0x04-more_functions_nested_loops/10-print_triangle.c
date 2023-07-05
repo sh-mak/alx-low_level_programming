ad11feb1dcbba4133748afb042c08f013fc53c26
sh-mak <shadicj30@gmail.com>
2023-07-05 04:44:35 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ * @size: the size of the triangle
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int size)
+{
+	int h, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				if ((h + b) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
