1ebd7d5d4e8ff87d9f4933f5c15d15204b21e52c
sh-mak <shadicj30@gmail.com>
2023-07-04 18:01:13 -0700

    prntnm

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9
- * only using _putchar twice
+ * print_numbers - print 0 to 9 only using _putchar twice
  * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
 	int nm;
 
-	for (nm = 0; nm <= 9; nm++)
-	{
-		_putchar(nm + '0');
+	do {
+		_putchar(nm + 48);
+		nm++;
 	}
+	while (nm >= 0 && nm <= 9);
 	_putchar('\n');
 }
