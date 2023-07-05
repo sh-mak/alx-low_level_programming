c7f555f9e76cc011f75735d14a4acbb0d49400cb
sh-mak <shadicj30@gmail.com>
2023-07-04 17:41:59 -0700

    printnum

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * print_numbers - prints the numbers, from 0 to 9
- *  _putchar: it has to be used twice in code
- * Return: the nubers since 0 up to 9
+ * only using _putchar twice
+ * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
