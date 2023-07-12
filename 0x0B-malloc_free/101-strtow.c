f53813ce18f17730ecb31ca4fce6b9b9b0e45dd2
sh-mak <shadicj30@gmail.com>
2023-07-12 08:51:55 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -49,7 +49,7 @@ char **strtow(char *str)
 		{
 		for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 			j++;
-		w[wc] = (char *)malloc(j * sizeof(char))
+		w[wc] = (char *)malloc(j * sizeof(char));
 			j--;
 		if (w[wc] == NULL)
 		{
