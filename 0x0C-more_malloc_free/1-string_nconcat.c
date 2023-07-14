7f12b7269868c2135905591bb205b917e2c29642
sh-mak <shadicj30@gmail.com>
2023-07-14 13:11:48 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,28 +11,36 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j, s1l, s2l;
+	unsigned int i = 0, j = 0, k = 0, l  = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (s1l = 0; s1[s1l] != '\0'; s1l++)
-		;
-	for (s2l = 0; s2[s2l] != '\0'; s2l++)
-		;
-	str = malloc(s1l + n + 1);
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-		s1[i] = s1[i];
-	for (j = 0; j < n; j++)
-	{
-		s1[i] = s2[j];
+	while (s1[i])
 		i++;
-	}
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
 
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+	while (j < i)
+	{
+		if (j <= i)
+		str[j] = s1[j];
+		if  (j >= i)
+		{
+		str[j] = s2[k];
+		k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
 	return (str);
 }
