765d576b5db192a6f4b1ac31f667c100dcbc4e03
sh-mak <shadicj30@gmail.com>
2023-07-14 10:55:24 -0700

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory
+ * @b: integer
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+	return (m);
+}
