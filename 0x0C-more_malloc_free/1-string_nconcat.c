824974e18daa4c2ce9a52921d31e5709f625d4a0
sh-mak <shadicj30@gmail.com>
2023-07-14 12:52:47 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j, s1l, s2l;
 
-	if (s1l == NULL)
-		s1l = "";
-	if (s2l == NULL)
-		s2l = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (s1l = 0; s1[s1l] != '\0'; s1l++)
 		;
 	for (s2l = 0; s2[s2l] != '\0'; s2l++)
@@ -33,6 +33,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1[i] = s2[j];
 		i++;
 	}
-	s1[i] != '\0';
+
 	return (str);
 }
