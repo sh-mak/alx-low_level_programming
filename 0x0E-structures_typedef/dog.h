5d0fe4f3e022cbcae7bc09dc5ce78dc1f629d782
sh-mak <shadicj30@gmail.com>
2023-07-18 16:40:03 -0700

    dog_h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
