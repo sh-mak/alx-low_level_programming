cf4ff3609143fac32f8ec07ed3ab638f72e05aab
sh-mak <shadicj30@gmail.com>
2023-07-19 11:49:39 -0700

    calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op+sub - subtracts two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: defference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_dev - devides two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: quotient
+ */
+int op_dev(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: remainders
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
