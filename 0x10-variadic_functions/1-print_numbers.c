fff4ab1dfa6d434116362fd91764169bb29fc741
sh-mak <shadicj30@gmail.com>
2023-07-20 10:39:51 -0700

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,6 +21,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	va_start(ap, n);
 	while (i--)
-		printf("%d\n", va_arg(ap, n), i ? (separator ? separator : "") : "\n");
+		printf("%d\n", va_arg(ap, int), i ? (separator ? separator : "") : "\n");
 	va_end(ap);
 }
