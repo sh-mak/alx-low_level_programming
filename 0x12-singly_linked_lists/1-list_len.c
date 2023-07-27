a082bbc914fd58b62965870bd924e1cccf24d2b7
sh-mak <shadicj30@gmail.com>
2023-07-27 10:56:45 -0700

    list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to first node
+ * Return: size of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
